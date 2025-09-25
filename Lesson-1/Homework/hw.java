006fae0f1c8d5eedc53901bd980d4f51f1d2f3b7
theWhiteHackers <nurefsanbera@gmail.com>
2025-09-25 08:49:45 -0500

    done!

--- Lesson-1/Homework/hw.java
+++ Lesson-1/Homework/hw.java
@@ -53,16 +53,73 @@ you can also sort the array numerically and alphabetically
 
 /*
 STACKS
+
+Linear data structure that follows the Last in First Out (LIFO) principle
+
+    import java.util.Stack
+    Stack<Integer> s = new Stack<>(); - creates an empty stack
+    s.push(1); - pushes elements onto the stack
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    s.pop() - removes and returns the top element from the stack
+    s.peek() - returns the top element from the stack
+    s.empty() - checks if the stack is empty
+    s.size() - finds the number of elements in the stack
+    s.search() - determines whether an obj exists in the stack and returns the position, if element is not found returns -1
+
 */
 
 /*
 QUEUE
+
+Elements are proccess in the order they were insterted, First In First Out (FIFO) principle
+
+    public interface Queue extends Collection
+    Queue<obj> q = new LinkedList<obj>(); - we cannot use queue directly since it is a interface
+                       new PriorityQueue
+                       new PriorityBlockingQueue
+
+    q.add() - adds element to the end, if q is full throws an exception
+    q.offer() - adds element to the end, if q is full returns false
+    q.remove()  - removes and returns element at the front of q
+    q.poll() - removes and returns element at the front of the queue, if empty returns null
+    q.element() - returns the element at the front w/o removing it, if q is empty returns an exception
+    q.peek() - returns the element at the front w/o removing it, if q is empty returns null
+
+
 */
 
 /*
 SUPPLIERS
+
+You don't need to suppy a value but you get a result
+
+    import java.util.function.Supplier
+
+        get() - the only function
+
+    Supplier<Double> randomValue = () -> Math.random();
+    System.out.println(randomValue.get());
+
 */
 
 /*
 HASHMAPS
+
+stores items in value/key pairs, where each key maps to a specific value
+
+import java.util.HashMap; -  import the HashMap class
+HashMap<String, String> captialCities = new HashMap<>();
+
+capitalCities.put("England", "London") - adds key/value pair
+capitalCities.get("England") - gets the value
+capitalCities.remvoe("England") - removes the pair
+capitalCities.clear() - removes everything
+capitalCities.size() - finds the number of items
+
+.keySet() for only the keys
+.value() for only the values
+
 */
